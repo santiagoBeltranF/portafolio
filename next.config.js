45beb00681eae0aa5bdf2421c62102f7fcc3701e
santiagobeltranf <santiago.beltran.7813@eam.edu.co>
2025-05-27 18:53:07 -0500

    deploy

--- next.config.js
+++ next.config.js
@@ -1,36 +1,39 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/portafolio' : undefined,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portafolio/' : undefined,
+    trailingSlash: true,
+
+    images: {
+        unoptimized: true,
+        domains: [
+            "source.unsplash.com",
+            "images.unsplash.com",
+            "ext.same-assets.com",
+            "ugc.same-assets.com",
+        ],
+        remotePatterns: [{
+                protocol: "https",
+                hostname: "source.unsplash.com",
+                pathname: "/",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                pathname: "/",
+            },
+            {
+                protocol: "https",
+                hostname: "ext.same-assets.com",
+                pathname: "/",
+            },
+            {
+                protocol: "https",
+                hostname: "ugc.same-assets.com",
+                pathname: "/",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
